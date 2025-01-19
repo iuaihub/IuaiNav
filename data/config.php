ee9860f69beaac966c0198287f89598ba057185a
iuaihub <996007@qq.com>
2025-01-19 17:45:37 +0800

    Add files via upload

---
+++ data/config.php
@@ -0,0 +1,11 @@
+<?php
+//数据库配置 更多免费源码请访问IUAI导航站 https://www.iuaiub.com
+$db_config = array(
+    "type" => "mysql", //类型
+	"host" => "localhost", //地址
+	"port" => 3306, //端口
+	"name" => "daohang3", //库名
+	"user" => "daohang3", //账号
+	"password" => "daohang3" //密码
+);
+?>
